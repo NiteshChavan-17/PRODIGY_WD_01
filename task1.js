97dbed3c0a217044c63c71c58147627987a34bb9
NiteshChavan-17 <chavannitesh963@gmail.com>
2025-06-17 16:27:01 +0530

    changes made

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
   if (window.scrollY > 50) {
-    navbar.style.backgroundColor = "#222"; 
+    navbar.style.backgroundColor = "#3984d4"; 
   } else {
     navbar.style.backgroundColor = "#333"; 
   }
